d6d8ad9ef34a6b3ef85e95e5e8db7a06204b3e62
KKKUBAKKK <kuba.kindracki@gmail.com>
2024-06-11 19:29:24 +0200

    4 timeouts, 2 incorrect colorings

--- LAB15_AiSD2/GraphColorer.cs
+++ LAB15_AiSD2/GraphColorer.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
+using System.Linq;
 using ASD.Graphs;
 
 namespace ASD2
@@ -16,72 +16,87 @@ namespace ASD2
         public (int numberOfColors, int[] coloring) FindBestColoring(Graph g)
         {
             int[] coloring = new int[g.VertexCount];
-            Array.Fill(coloring, -1);
+            for (int i = 0; i < g.VertexCount; i++)
+                coloring[i] = -1;
 
-            int numberOfColors;
-            for (numberOfColors = 1; numberOfColors <= g.VertexCount; numberOfColors++)
-            {
-                if (GreedyColoring(g, 0, coloring, numberOfColors))
-                    break;
-            }
-
-            return (numberOfColors, coloring);
+            var res = FindBestColoringRec(g, 0, coloring, 0);
+            return res;
         }
 
-        public bool[] AvailableColors(Graph g, int v, int[] coloring, int numberOfColors)
+        // Funkcja dziala w ten sposob ze jesli nie znajdzie 0 <= kolor < numberOfColors, to zwraca nowy
+        public (int color, int n, int f, bool[] used) FindSmallestAvailableColor(Graph g, int v, int[] coloring, int numberOfColors)
         {
             bool[] used = new bool[numberOfColors];
-
-            foreach (var n in g.OutNeighbors(v))
+            int n = 0;
+            foreach (var neighbor in g.OutNeighbors(v))
             {
-                if (coloring[n] != -1)
-                    used[coloring[n]] = true;
+                if (coloring[neighbor] != -1)
+                    used[coloring[neighbor]] = true;
+                else
+                    n++;
             }
 
-            return used;
+            int f = used.Select((b => (b) ? 0 : 1)).Sum();
+            
+            for (int i = 0; i < used.Length; i++)
+                if (!used[i])
+                    return (i, n, f, used);
+
+            return (numberOfColors, n, f, used);
         }
 
-        public bool GreedyColoring(Graph g, int v, int[] coloring, int numberOfColors)
+        public (int numberOfColors, int[] coloring) FindBestColoringRec(Graph g, int v, int[] coloring, 
+            int numberOfColors, int maxNumberOfColors = Int32.MaxValue)
         {
             if (v == g.VertexCount)
-                return true;
+                return (numberOfColors, coloring);
             
-            var used = AvailableColors(g, v, coloring, numberOfColors);
+            (int color, int n, int f, bool[] used) c = FindSmallestAvailableColor(g, v, coloring, numberOfColors);
+            // if (c.color >= maxNumberOfColors)
+            //     return (Int32.MaxValue, new int[0]);
             
-            int n = g.OutNeighbors(v).Select((i => (coloring[i] == -1) ? 1 : 0)).Sum();
-            int c = used.Select((b => (b) ? 0 : 1)).Sum();
-
-            if (n < c)
+            if (c.n < c.f)
             {
-                if (GreedyColoring(g, v + 1, coloring, numberOfColors))
-                {
-                    used = AvailableColors(g, v, coloring, numberOfColors);
-                    for (int i = 0; i < used.Length; i++)
-                        if (!used[i])
-                        {
-                            coloring[v] = i;
-                            break;
-                        }
-
-                    return true;
-                }
-
-                return false;
+                (int numberOfColors, int[] coloring) temp = FindBestColoringRec(g, v + 1, coloring, numberOfColors);//, maxNumberOfColors);
+                // if (temp.numberOfColors >= maxNumberOfColors)
+                //     return (Int32.MaxValue, new int[0]);
+                (int color, int n, int f, bool[] used) t =
+                    FindSmallestAvailableColor(g, v, temp.coloring, temp.numberOfColors);
+                // if (t.color >= temp.numberOfColors) // chyba niepotrzebne bo i tak juz mial wiecej kolorow niz potrzebuje
+                //     temp.numberOfColors++;
+                var res = (int[])temp.coloring.Clone();
+                res[v] = t.color;
+                
+                return (temp.numberOfColors, res);
             }
-
-            for (int i = 0; i < used.Length; i++)
+            
+            // teraz wywolywanie na rozne sposoby i branie najlepszego, czyli w sumie co?
+            // najpierw koloruje wierzcholek najmniejszym mozliwym kolorem i w razie potrzeby
+            // coloring[v] = c.color;
+            (int numberOfColors, int[] coloring) best = (int.MaxValue, new int[0]);
+            int tf = 0;
+            if (c.color == numberOfColors)
+                numberOfColors++;
+            for (int i = c.color; i < numberOfColors; i++)
             {
-                if (used[i])
+                if (tf > c.f)
+                    break;
+                
+                if (i < c.used.Length && c.used[i])
                     continue;
-
+                
+                tf++;
                 coloring[v] = i;
-                if (GreedyColoring(g, v + 1, coloring, numberOfColors))
-                    return true;
+                (int numberOfColors, int[] coloring) temp = FindBestColoringRec(g, v + 1, coloring, numberOfColors);//, Math.Min(best.numberOfColors, maxNumberOfColors));
+                if (temp.numberOfColors < best.numberOfColors)
+                {
+                    best.numberOfColors = temp.numberOfColors;
+                    best.coloring = (int[])temp.coloring.Clone();
+                }
                 coloring[v] = -1;
             }
-
-            return false;
+            
+            return best;
         }
-        
     }
 }
